34327f7a98daf5f6e5bf5c5ffb9f9fe7a1353815
Martin Blanco Zamora <165429232+MartinBlancoZamora@users.noreply.github.com>
2025-06-29 11:41:25 -0300

    Modificaciones

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -4,9 +4,9 @@ using namespace cv;
 
 #define FLOAT_TO_INT(x) ((x) >= 0 ? (int)((x) + 0.5) : (int)((x) - 0.5))
 
-int procesarImagen(uchar *p, int nRows, int nCols, int channels);
-double valorRGBlineal(double RGBcomprimido);
-double valorYcomprimido(double valorYlineal);
+extern "C" double valorRGBlineal(double RGBcomprimido);
+extern "C" double valorYcomprimido(double valorYlineal);
+extern "C" int procesarImagen(uchar* p, int nRows, int nCols, int channels);
 
 int main(int argc, char **argv)
 {
@@ -50,72 +50,4 @@ int main(int argc, char **argv)
 
 	waitKey(0);
 	return (0);
-}
-
-int procesarImagen(uchar *p, int nRows, int nCols, int channels)
-{
-
-	int i, j;
-	double valorR, valorG, valorB;	  
-	double Rlineal, Glineal, Blineal; 
-	double Ylineal, Yrgb;
-
-	for (i = 0; i < nRows; i++)
-	{
-		for (j = 0; j < nCols * channels; j += 3)
-		{
-			
-			valorB = (((double)(*(p + j + i * nCols * channels))) / 255.0);
-			Blineal = valorRGBlineal(valorB);
-			
-			valorG = (((double)(*(p + j + i * nCols * channels + 1))) / 255.0);
-			Glineal = valorRGBlineal(valorG);
-			
-			valorR = (((double)(*(p + j + i * nCols * channels + 2))) / 255.0);
-			Rlineal = valorRGBlineal(valorR);
-			
-			Ylineal = 0.2126 * Rlineal + 0.7152 * Glineal + 0.0722 * Blineal;
-			
-			Yrgb = 255 * valorYcomprimido(Ylineal);
-			
-			*(p + j + i * nCols * channels) = FLOAT_TO_INT((Yrgb));
-			*(p + j + i * nCols * channels + 1) = FLOAT_TO_INT((Yrgb));
-			*(p + j + i * nCols * channels + 2) = FLOAT_TO_INT((Yrgb));
-		}
-	}
-	return (0);
-}
-
-double valorRGBlineal(double RGBcomprimido)
-{
-	double resultado;
-	double a, b;
-	if (isless(RGBcomprimido, 0.04045))
-	{
-		resultado = RGBcomprimido / 12.92;
-	}
-	else
-	{
-		a = (RGBcomprimido + 0.055);
-		b = (a / 1.055);
-		resultado = pow(b, (2.4));
-	}
-	return (resultado);
-}
-
-double valorYcomprimido(double valorYlineal)
-{
-	double resultado;
-	double a, b;
-	if (isless(valorYlineal, 0.0031308))
-	{
-		resultado = valorYlineal * 12.92;
-	}
-	else
-	{
-		a = pow(valorYlineal, (1 / 2.4));
-		b = 1.055 * a;
-		resultado = b - 0.055;
-	}
-	return (resultado);
 }
